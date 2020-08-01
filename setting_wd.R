bca323bb59cc8f62ec4d6c558edc82e86cb6dc8e
JACKMCDONNELL <mcdonnell.jack@gmail.com>
2020-08-01 18:30:13 -0400

    committ

---
+++ setting_wd.R
@@ -0,0 +1,11 @@
+getwd()
+read.csv("/Users/johnmcdonnell/Desktop/R Programming for Data Science/datasets/specdata/001.csv")
+myfunction <- function(x) {
+  y <- rnorm(100)
+  mean(y)
+}
+
+myfunction()
+dir()
+ls()
+second(4:10)
