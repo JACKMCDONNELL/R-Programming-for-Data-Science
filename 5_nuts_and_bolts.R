b74cae904c1bf7f0d24449b42a8dd95e60049628
JACKMCDONNELL <mcdonnell.jack@gmail.com>
2020-08-02 15:09:17 -0400

    finished rest of written chapter 5

--- 5_nuts_and_bolts.R
+++ 5_nuts_and_bolts.R
@@ -28,4 +28,88 @@ as.numeric(x)
 as.logical(x)
 as.character(x)
 
+# matrix
+m <- matrix(nrow=2, ncol=3)
+m
+dim(m)
+attributes(m)
+
+m <- matrix(1:6, nrow=2, ncol=3)
+m
+
+
+m <- 1:10
+m
+
+dim(m) <- c(2,5)
+m
+
+# can bind columns or rows
+
+x <- 1:3
+y <- 10:12
+
+cbind(x,y)
+rbind(x,y)
+
+# lists
+
+x <- list(1, "a", TRUE, 1 + 4i)
+x
+
+x <- vector("list", length=5)
+x
+
+# factors
+
+x <- factor(c("yes", "yes", "no", "yes", "no"))
+x
+table(x)
+
+# see the underlying representation of factor
+unclass(x)
+
+# missing values
+
+x <- c(1, 2, NA, 10, 3)
+is.na(x)
+is.nan(x)
+
+x <- c(1, 2, NaN, NA, 4)
+is.na(x)
+
+# so all NaN are NA, but not the other way around
+is.nan(x)
+
+# data frames
+
+x <- data.frame(foo = 1:4, bar = c(T, T, F, F))
+x
+
+nrow(x)
+ncol(x)
+
+# names
+
+x <- 1:3
+names(x)
+
+names(x) <- c("New York", "Seattle", "Los Angeles")
+
+x
+
+names(x)
+
+x <- list("Los Angeles" = 1, Boston = 2, London = 3)
+x
+
+
+m <- matrix(1:4, nrow=2, ncol=2)
+dimnames(m) <- list(c("a", "b"), c("c","d"))
+m
+
+colnames(m) <- c("h","f")
+rownames(m) <- c("x","z")
+m
+
 
