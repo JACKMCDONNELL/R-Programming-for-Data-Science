e25fff4527a9aba99cf9f8f5d7f3ab67418574fe
JACKMCDONNELL <mcdonnell.jack@gmail.com>
2020-08-30 16:42:50 -0400

    needed changes

--- 14_control_structures.R
+++ 14_control_structures.R
@@ -106,3 +106,53 @@ for(i in 1:100) {
   }
 }
 
+# functions
+
+f <- function() {
+   ## This is an empty function
+}
+
+## Functions have their own class
+class(f)
+
+## Execute this function
+f()
+
+# simple function for adding two numbers
+add2 <- function(x,y) {
+  x + y
+}
+add2(3,5)
+
+above10 <- function(x) {
+  use <- x > 10
+  x[use]
+}
+
+above <- function(x,n) {
+    use <- x > n
+    x[use]
+}
+
+above(15,2)
+
+above <- function(x,n=10) {
+  use <- x > n
+  x[use]
+}
+
+above(11)
+
+columnmean <- function(y, removeNA = TRUE) {
+    nc <- ncol(y)
+    means <- numeric(nc)
+    for(i in 1:nc) {
+        means[i] <- mean(y[, i], na.rm = removeNA)
+    }
+    means
+}
+
+x <- matrix(1:6, 2, 3)
+
+columnmean(x)
+
