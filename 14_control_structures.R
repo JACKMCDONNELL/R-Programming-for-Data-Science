f721ea766bbe1196b451bd52adb5c2c6d92af26d
JACKMCDONNELL <mcdonnell.jack@gmail.com>
2020-09-13 17:02:53 -0400

    additions

--- 14_control_structures.R
+++ 14_control_structures.R
@@ -2,38 +2,38 @@
 
 x <- runif(1,0,10)
 if(x > 3) {
-    y <- 10
+  y <- 10
 } else {
-    y <- 0 
+  y <- 0 
 }
 
 # equivalently
 
 y <- if(x >3) {
-    10
+  10
 } else {
-    0
+  0
 }
 
 ## FOR LOOPS
 
 for(i in 1:10) {
-    print(i)
+  print(i)
 }
 
 x <- c("a", "b", "c", "d")
 
 for(i in 1:4) {
   ## Print out each element of 'x'
-   print(x[i])
+  print(x[i])
 }
 
 for(i in seq_along(x)) {
-    print(x[i])
+  print(x[i])
 }
 
 for(letter in x) {
-    print(letter)
+  print(letter)
 }
 
 for(i in 1:4) print(x[i]) # can omit curly braces in one-line loops
@@ -43,9 +43,9 @@ for(i in 1:4) print(x[i]) # can omit curly braces in one-line loops
 x <- matrix(1:6, 2, 3)
 
 for(i in seq_len(nrow(x))) {
-    for(j in seq_len(ncol(x))) {
-      print(x[i, j])
-    }
+  for(j in seq_len(ncol(x))) {
+    print(x[i, j])
+  }
 }
 
 # WHILE LOOPS
@@ -53,8 +53,8 @@ for(i in seq_len(nrow(x))) {
 count <- 0
 
 while(count <10) {
-    print(count)
-    count <- count + 1
+  print(count)
+  count <- count + 1
 }
 
 z <- 5
@@ -64,9 +64,9 @@ while(z >= 3 && z <= 10) {
   coin <- rbinom(1, 1, 0.5)
   
   if(coin == 1) { ## random walk
-      z <- z + 1
+    z <- z + 1
   } else {
-      z <- z - 1
+    z <- z - 1
   }
 }
 
@@ -81,9 +81,9 @@ repeat {
   x1 <- computeEstimate()
   
   if(abs(x1 - x0) < tol) { ## Close enough?
-      break
+    break
   } else {
-      x0 <- x1
+    x0 <- x1
   }
 }
 
@@ -91,8 +91,8 @@ repeat {
 
 for(i in 1:100) {
   if(i <= 20) {
-      ## Skip the first 20 iterations
-      next
+    ## Skip the first 20 iterations
+    next
   }
   ## Do something here
 }
@@ -101,15 +101,15 @@ for(i in 1:100) {
   print(i)
   
   if(i > 20) {
-      ## Stop loop after 20 iterations
-      break
+    ## Stop loop after 20 iterations
+    break
   }
 }
 
 # functions
 
 f <- function() {
-   ## This is an empty function
+  ## This is an empty function
 }
 
 ## Functions have their own class
@@ -130,8 +130,8 @@ above10 <- function(x) {
 }
 
 above <- function(x,n) {
-    use <- x > n
-    x[use]
+  use <- x > n
+  x[use]
 }
 
 above(15,2)
@@ -144,12 +144,12 @@ above <- function(x,n=10) {
 above(11)
 
 columnmean <- function(y, removeNA = TRUE) {
-    nc <- ncol(y)
-    means <- numeric(nc)
-    for(i in 1:nc) {
-        means[i] <- mean(y[, i], na.rm = removeNA)
-    }
-    means
+  nc <- ncol(y)
+  means <- numeric(nc)
+  for(i in 1:nc) {
+    means[i] <- mean(y[, i], na.rm = removeNA)
+  }
+  means
 }
 
 x <- matrix(1:6, 2, 3)
@@ -163,13 +163,13 @@ f <- function(a, b=1, c=2, d=NULL){
 # lazy evaluation
 
 f <- function(a,b) {
-    a^2
+  a^2
 }
 f(2) # no error, because b is never used anyways
 
 f <- function(a, b) {
-    print(a)
-    print(b)
+  print(a)
+  print(b)
 }
 
 f(45)
@@ -183,10 +183,50 @@ search()
 # lexical scoping
 
 f <- function(x,y) {
-    x^2 + y / z # note that z does not have an assigned value - it is a free variable
+  x^2 + y / z # note that z does not have an assigned value - it is a free variable
 }
 
 # what value gets assigned to z?
 # depends on values in the parent environment. R just keeps going down sequence of parent environments
 # if can't find the value, throws an error
 f(2,5)
+
+# optimization in R
+
+make.NegLogLik <- function(data, fixed = c(FALSE, FALSE)) {
+  params <- fixed
+  function(p) {
+    params[!fixed] <- p
+    mu <- params[1]
+    sigma <- params[2]
+    
+    ## Calculate the Normal density
+    a <- -0.5*length(data)*log(2*pi*sigma^2)
+    b <- -0.5*sum((data-mu)^2) / (sigma^2)
+    -(a + b)
+  }
+}
+
+
+set.seed(1)
+normals <- rnorm(100,1,2)
+nLL <- make.NegLogLik(normals)
+nLL
+
+ls(environment(nLL))
+
+optim(c(mu = 0, sigma = 1), nLL)$par
+
+nLL <- make.NegLogLik(normals, c(FALSE, 2))
+optimize(nLL, c(-1, 3))$minimum
+
+nLL <- make.NegLogLik(normals, c(1, FALSE))
+optimize(nLL, c(1e-6, 10))$minimum
+
+## Fix 'mu' to be equalt o 1
+nLL <- make.NegLogLik(normals, c(1, FALSE))
+x <- seq(1.7, 1.9, len = 100)
+
+## Evaluate 'nLL()' at every point in 'x'
+y <- sapply(x, nLL)
+plot(x, exp(-(y - min(y))), type = "l")
